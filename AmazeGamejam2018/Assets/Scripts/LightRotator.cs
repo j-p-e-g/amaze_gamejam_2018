c5da333e50340a466417435217a3895f99cf18af
3real <mikegrahameverett@gmail.com>
2018-04-28 17:23:44 +0200

    Added 5 prefabs, the corresponding models and scripts and test scene. added DS_Store as we're not sure if it's important. We believe it's basically metadata

---
+++ AmazeGamejam2018/Assets/Scripts/LightRotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotator : MonoBehaviour {
+
+	public float speed = 200f;
+	public GameObject HeadAxis;
+	private Rigidbody myRigidbody;
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	
+	void Update () {
+		
+		float h = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		myRigidbody.AddTorque(transform.up * h);
+
+	
+	}
+}
---
+++ AmazeGamejam2018/Assets/Scripts/bladeRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bladeRotator : MonoBehaviour {
+
+	public float amount = 200f;
+
+	
+
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+			{
+			transform.Rotate(transform.up * amount * Time.deltaTime, Space.World);
+			}
+	}
+}
