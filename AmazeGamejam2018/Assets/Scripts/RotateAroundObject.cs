bf3293b526a4600729f72e62c00da1d2196246e4
j-p-e-g <j-p-e-g@users.noreply.github.com>
2018-04-28 16:11:41 +0200

    Basic scene and rotation script

---
+++ AmazeGamejam2018/Assets/Scripts/RotateAroundObject.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundObject : MonoBehaviour
+{
+	public GameObject centerObject;
+    public float speed = 100;
+    public float initialTimeOffset = 15;
+
+    private Transform initialTransform;
+
+	void Start ()
+    {
+        initialTransform = this.transform;
+        this.transform.LookAt(centerObject.transform);
+        this.DoRotate(initialTimeOffset);
+	}
+
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        HandlePlayerInput(Time.deltaTime);
+    }
+
+    private void HandlePlayerInput(float time)
+    {
+        if (Input.GetAxis("Submit") == 1)
+        {
+			Debug.Log("entered Return");
+            this.ResetTransform();
+        }
+        else
+        {
+            float translation = Input.GetAxis("Horizontal") * speed;
+			Debug.Log("translation: " + translation);
+            this.DoRotate(translation);
+        }
+    }
+
+    private void DoRotate(float speed)
+    {
+        transform.RotateAround(centerObject.transform.position, Vector3.up, speed);
+    }
+
+    void ResetTransform()
+    {
+        this.transform.position = initialTransform.position;
+        this.transform.rotation = initialTransform.rotation;
+    }
+}
